21235dcf01c8eaef79af7963f8c9a3d282c714fe
collin banks <collin@CollinMBPro15.home>
2018-06-13 16:25:32 -0400

    service worker now caches all sites to be used offline

--- sw.js
+++ sw.js
@@ -1,25 +1,31 @@
+var urls = [
+  '/',
+  'index.html',
+  'restaurant.html',
+  'img',
+  'css/styles.css',
+  'data/restaurants.json',
+  'js/main.js',
+  'js/dbhelper.js',
+  'js/restaurant_info.js',
+];
+
+for(let i=1; i<=10; i++){
+  urls.push(`restaurant.html?id=${i}`);
+  }
+
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('mws-restaurant-stage-1').then(function(cache) {
-        return cache.addAll([
-          '/',
-          'index.html',
-          'restaurant.html',
-          'img',
-          'css/styles.css',
-          'data/restaurants.json',
-          'js/main.js',
-          'js/dbhelper.js',
-          'js/restaurant_info.js',
-        ]);
-      })
-    );
-  });
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response){
-        if(response) return response;
-        return fetch(event.request);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('mws-restaurant-stage-1').then(function(cache) {
+      return cache.addAll(urls);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      if(response) return response;
+      return fetch(event.request);
+    })
+  );
+});
