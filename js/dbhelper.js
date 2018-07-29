d80a9340b7b8f9a32702d011191898ab67f561c5
jazztbone2b <jazztbone2b@gmail.com>
2018-07-29 12:17:36 -0400

    added idb library and stored restaurant json in indexeddb

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,28 +7,31 @@ class DBHelper {
    * Database URL.
    * Change this to restaurants.json file location on your server.
    */
+  //change the port to 1337 to get info from the server
   static get DATABASE_URL() {
-    const port = '8000'; // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337; // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
+    fetch(DBHelper.DATABASE_URL, { method: 'GET'})
+      .then(function(response) {
+        if (response.status !== 200) {
+          console.log('Something went wrong... Status Code: ' + response.status);
+          return;
+        }
+        
+        response.json().then(function(restaurants) {
+          //console.log(restaurants);
+          callback(null, restaurants);
+        }).catch(function(err) {
+          console.log('Fetch error', err);
+        });
       }
-    };
-    xhr.send();
+    )
   }
 
   /**
@@ -150,7 +153,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /**
--- sw.js
+++ sw.js
@@ -1,29 +1,79 @@
-var urls = [
+//Cache the JSON responses for offline use by using the IndexedDB API
+importScripts('../idb.js');
+
+const restaurantsURL = 'http://localhost:1337/restaurants';
+
+const dbPromise = idb.open('mws-restaurants', 2, (upgradeDb) => {
+  switch(upgradeDb.oldVersion) {
+    case 0:
+      upgradeDb.createObjectStore('restaurants', { keyPath: 'id' });
+  }
+});
+
+//get the url for the restaurants
+fetch(restaurantsURL)
+  .then((response) => {
+    //return the response as json
+    return response.json();
+  })
+  .then((restaurants) => {
+    //put the json into the restaurants store
+    dbPromise.then((db) => {
+      let tx = db.transaction('restaurants', 'readwrite');
+      let keyValStore = tx.objectStore('restaurants');
+      restaurants.forEach((restaurant) => {
+        keyValStore.put(restaurant);
+      });
+      return tx.complete;
+      }).then(() => {
+        console.log('Restaurant info added to idb');
+      }).catch((err) => {
+        console.log(`Woops... Error status: ${err}`);
+      });
+    });
+
+//deliver the content from idb cache if network is unavailable
+dbPromise.then((db) => {
+  let tx = db.transaction('restaurants', 'readonly');
+  let restStore = tx.objectStore('restaurants');
+  console.log('getting items...');
+  return restStore.getAll();
+}).then((items) => {
+  console.log(items);
+}).catch((err) => {
+  console.log(`Woops... Error status ${err}`);
+});
+
+
+let urls = [
   '/',
   'index.html',
   'restaurant.html',
   'img',
   'css/styles.css',
-  'data/restaurants.json',
   'js/main.js',
   'js/dbhelper.js',
   'js/restaurant_info.js',
+  'sw.js',
+  'idb.js'
 ];
 
-for(let i=1; i<=10; i++){
+for(let i=1; i<=10; i++) {
   urls.push(`restaurant.html?id=${i}`);
   }
 
-self.addEventListener('install', function(event) {
+const staticCacheName = 'mws-restaurants-v2';
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('mws-restaurant-stage-1').then(function(cache) {
+    caches.open(staticCacheName).then((cache) => {
       return cache.addAll(urls);
     })
   );
 });
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response){
+    caches.match(event.request).then((response) => {
       if(response) return response;
       return fetch(event.request);
     })
